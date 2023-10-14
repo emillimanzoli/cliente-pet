2d996c7b2deb27acbe66b3c66e980b8a0c2eee80
Emilli Manzoli <emillimanzoli@gmail.com>
2023-10-14 00:42:28 -0300

    implementa ClienteApplicationService e novo construtor Cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,8 +1,10 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import lombok.Builder;
 import lombok.Value;
 
 @Value
+@Builder
 public class ClienteResponse {
     private long idCliente;
 }

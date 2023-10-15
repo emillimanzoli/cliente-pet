4497b5b2571fb31cd0762779aa874bfcfb67b258
Emilli Manzoli <emillimanzoli@gmail.com>
2023-10-15 18:51:08 -0300

    implementa fluxo repository, teste OK

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -1,11 +1,13 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.domain.Cliente;
 import br.com.petz.clientepet.cliente.domain.Sexo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class ClienteListResponse {
     private Long idCliente;
@@ -14,4 +16,7 @@ public class ClienteListResponse {
     private String email;
     private String celular;
 
+    public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+        return null;
+    }
 }
